06a37216a1df471d82f1f34fe3860e7f3f1b5543
Aayam Neupane <aayam@teslatech.com.np>
2023-03-19 00:02:16 +0545

    player moving mechanism setup

--- player.gd
+++ player.gd
@@ -1,6 +1,38 @@
 extends Node2D
 
+@onready var player_skin: TextureRect = $PlayerSkin
+
+var screen_size : Vector2
+
+var move_speed := 100
+
 
 func _ready() -> void:
-	pass
+	screen_size = get_viewport_rect().size
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	var velocity := Vector2.ZERO
 	
+	if event.is_action_pressed("key_left"):
+		# move left
+		velocity.x -= 1
+	
+	if event.is_action_pressed("key_right"):
+		# move right
+		velocity.x += 1
+	
+	if event.is_action_pressed("key_up"):
+		# move up
+		velocity.y -= 1
+		
+	if event.is_action_pressed("key_down"):
+		# move down
+		velocity.y += 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * move_speed
+	
+	position += velocity
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
